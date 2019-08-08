c5b5454d67c1495eb75907072b888942f6f6af5c
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-08-08 13:20:18 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Import css modules stylesheet as styles
 
-const App= ({title}) =>(
-  <div className="header">{title}</div>
-)
+class Card extends React.Component {
+  render() {
+    return (
+      <div ClassName="github-profile">
+        <img src="https://placehold.it/75" />
+        <div className="info">
+          <div className="name">name here</div>
+          <div className="company">company here</div>
+        </div>
+      </div>
+    )
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card />
+      </div>
+    )
+  }
+}
 export default App;
